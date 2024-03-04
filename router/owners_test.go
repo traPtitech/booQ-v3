2a6ff4af195d1d1038a99a3ef263a39fad34f8ed
s9-sukyu <63638257+s9-sukyu@users.noreply.github.com>
2024-03-05 00:07:34 +0900

    実装中

---
+++ router/owners_test.go
@@ -0,0 +1,13 @@
+package router
+
+import "testing"
+
+func TestPostOwners(t *testing.T) {
+	t.Parallel()
+}
+func TestPatchOwners(t *testing.T) {
+	t.Parallel()
+}
+func TestDeleteOwners(t *testing.T) {
+	t.Parallel()
+}
