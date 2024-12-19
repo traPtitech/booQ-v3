5c23dfcb57cf259a37b678eedcec61caaf493bd8
cp-20 <n.naoki.1587@gmail.com>
2024-12-19 16:27:59 +0900

    コメント周りのAPIを実装

--- router/comments.go
+++ router/comments.go
@@ -1,12 +1,44 @@
 package router
 
 import (
-	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/booQ-v3/model"
 )
 
-// PostComments POST /items/:id/comments
-func PostComments(c echo.Context) error {
-	return echo.NewHTTPError(http.StatusNotImplemented, "Not Implemented")
+type PostCommentBody struct {
+	Comment string `json:"comment"`
+}
+
+type PostCommentResponse struct {
+	ID int `json:"id"`
+}
+
+// PostComment POST /items/:id/comments
+func PostComment(c echo.Context) error {
+	itemIDRaw := c.Param("id")
+	itemID, err := strconv.Atoi(itemIDRaw)
+	if err != nil {
+		return invalidRequest(c, err)
+	}
+
+	me := getAuthorizedUser(c)
+
+	var body PostCommentBody
+	if err := c.Bind(&body); err != nil {
+		return invalidRequest(c, err)
+	}
+
+	payload := model.CreateCommentPayload{
+		ItemID:  itemID,
+		UserID:  me,
+		Comment: body.Comment,
+	}
+	comment, err := model.CreateComment(&payload)
+	if err != nil {
+		return internalServerError(c, err)
+	}
+
+	return c.JSON(201, PostCommentResponse{ID: comment.ID})
 }
