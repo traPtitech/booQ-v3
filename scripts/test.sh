aad86b6c279df8abc3d8f48df004fdec2b271afc
kavos <kavos@example.com>
2025-07-09 23:01:28 +0900

    feat: local test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,9 @@
-docker compose -f docker/test/docker-compose.yml up --abort-on-container-exit
+docker compose -f docker/test/docker-compose.yml up -d
+
+export MYSQL_DATABASE=booq-test
+export MYSQL_PORT=3307
+
+go test . ./model -v -covermode=atomic -vet=off
+go test . ./router -v -covermode=atomic -vet=off
+
+docker compose -f docker/test/docker-compose.yml down
