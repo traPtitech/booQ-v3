863fa6e9aecb786fd3dfb3a5bba27704f9ea9572
kavos <kavos@example.com>
2025-07-09 23:18:16 +0900

    feat: test scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,21 @@
-docker compose -f docker/test/docker-compose.yml up -d
+docker compose -f docker/test/docker-compose.yml up -d --wait
 
 export MYSQL_DATABASE=booq-test
 export MYSQL_PORT=3307
 
-go test . ./model -v -covermode=atomic -vet=off
-go test . ./router -v -covermode=atomic -vet=off
+if [ "$1" = "cover" ]; then
+  echo "Running Tests With Coverage..."
+
+  go test . ./model -v -covermode=atomic -cover -coverprofile=cover_model.out
+  go test . ./router -v -covermode=atomic -cover -coverprofile=cover_router.out
+
+  go tool cover -html=cover_model.out -o cover_model.html
+  go tool cover -html=cover_router.out -o cover_router.html
+else
+  echo "Running Tests..."
+
+  go test . ./model -v -covermode=atomic
+  go test . ./router -v -covermode=atomic
+fi
 
 docker compose -f docker/test/docker-compose.yml down
