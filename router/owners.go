f8113e353924fb450c8a17047b11643962588a21
kavos <kavos@example.com>
2025-08-27 10:09:17 +0900

    feat: post owners handler

--- router/owners.go
+++ router/owners.go
@@ -2,13 +2,31 @@ package router
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/booQ-v3/model"
 )
 
 // PostOwners POST /items/:id/owners
 func PostOwners(c echo.Context) error {
-	return echo.NewHTTPError(http.StatusNotImplemented, "Not Implemented")
+	itemId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return invalidRequest(c, err)
+	}
+
+	owner := model.OwnershipPayload{}
+	if err := c.Bind(&owner); err != nil {
+		return invalidRequest(c, err)
+	}
+	owner.ItemID = itemId
+
+	res, err := model.CreateOwnership(owner)
+	if err != nil {
+		return internalServerError(c, err)
+	}
+
+	return c.JSON(http.StatusOK, res)
 }
 
 // PatchOwners PUT /items/:id/owners/:ownershipid
