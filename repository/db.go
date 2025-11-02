f758feec6f69ec2f37696410e0444a698fe0aa58
kavos113 <kohei.hadano@gmail.com>
2025-11-02 17:59:39 +0900

    fix: timezone

--- repository/db.go
+++ repository/db.go
@@ -5,7 +5,8 @@ import (
 	"os"
 	"time"
 
-	"gorm.io/driver/mysql"
+	"github.com/go-sql-driver/mysql"
+	gormMysql "gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -34,14 +35,24 @@ func NewDB(db *gorm.DB) *DB {
 }
 
 func EstablishConnection() (*gorm.DB, error) {
-	user := os.Getenv("MYSQL_USERNAME")
-	pass := os.Getenv("MYSQL_PASSWORD")
-	host := os.Getenv("MYSQL_HOST")
-	port := os.Getenv("MYSQL_PORT")
-	name := os.Getenv("MYSQL_DATABASE")
+	loc, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		return nil, err
+	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Asia%2FTokyo&charset=utf8mb4", user, pass, host, port, name)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	cfg := mysql.Config{
+		User:                 os.Getenv("MYSQL_USERNAME"),
+		Passwd:               os.Getenv("MYSQL_PASSWORD"),
+		Net:                  "tcp",
+		Addr:                 fmt.Sprintf("%s:%s", os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT")),
+		DBName:               os.Getenv("MYSQL_DATABASE"),
+		ParseTime:            true,
+		Loc:                  loc,
+		Collation:            "utf8mb4_general_ci",
+		AllowNativePasswords: true,
+	}
+
+	db, err := gorm.Open(gormMysql.Open(cfg.FormatDSN()), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
