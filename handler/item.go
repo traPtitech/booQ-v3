edd5f16c56f57529e45c2e798e892e788b691fe9
kavos113 <kohei.hadano@gmail.com>
2025-11-02 13:13:42 +0900

    fix: handling item not found err

--- handler/item.go
+++ handler/item.go
@@ -1,17 +1,22 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/booQ-v3/domain"
 	"github.com/traPtitech/booQ-v3/handler/openapi"
 )
 
 func (h *Handler) GetItem(ctx echo.Context, itemId openapi.ItemIdInPath) error {
 	item, err := h.iu.GetItemByID(itemId)
 	if err != nil {
-		// TODO: NotFoundなどのエラーも考慮する
-		return ctx.JSON(http.StatusInternalServerError, err)
+		if errors.Is(err, domain.ErrItemNotFound) {
+			return ctx.NoContent(http.StatusNotFound)
+		}
+		return ctx.JSON(http.StatusInternalServerError, fmt.Sprintf("failed to get item: %v", err))
 	}
 
 	return ctx.JSON(http.StatusOK, item)
