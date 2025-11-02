14576ae45405c020340d136acf4e4fb04785dc1d
kavos113 <kohei.hadano@gmail.com>
2025-11-02 14:54:11 +0900

    fix: add time to item model

--- handler/converter.go
+++ handler/converter.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"time"
+
+	"github.com/oapi-codegen/nullable"
 	"github.com/traPtitech/booQ-v3/domain"
 	"github.com/traPtitech/booQ-v3/handler/openapi"
 )
@@ -10,13 +13,23 @@ func toOpenAPIItem(domainItem *domain.Item) *openapi.Item {
 		return nil
 	}
 
+	var deletedAt nullable.Nullable[time.Time]
+	if domainItem.DeletedAt != nil {
+		deletedAt = nullable.NewNullableWithValue(*domainItem.DeletedAt)
+	} else {
+		deletedAt = nullable.NewNullNullable[time.Time]()
+	}
+
 	// TODO: add tags, comments, likes, transactions, etc.
 	return &openapi.Item{
-		Id:          &domainItem.ID,
+		Id:          domainItem.ID,
 		Name:        domainItem.Name,
 		Description: domainItem.Description,
 		ImgUrl:      domainItem.ImgUrl,
 		IsBook:      domainItem.BookDetail != nil,
 		IsTrapItem:  domainItem.EquipmentDetail != nil,
+		CreatedAt:   domainItem.CreatedAt,
+		UpdatedAt:   domainItem.UpdatedAt,
+		DeletedAt:   deletedAt,
 	}
 }
