4664041e9c50cbeb34e5bd5db97b864bf6776d4b
kavos113 <kohei.hadano@gmail.com>
2025-11-02 19:42:29 +0900

    update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,22 +1,11 @@
-#!/bin/sh
-docker compose -f docker/test/docker-compose.yml up -d --wait
-
-export MYSQL_DATABASE=booq-test
-export MYSQL_PORT=3307
-
+#!/bin/bash
 if [ "$1" = "cover" ]; then
   echo "Running Tests With Coverage..."
 
-  go test . ./model -v -covermode=atomic -cover -coverprofile=cover_model.out
-  go test . ./router -v -covermode=atomic -cover -coverprofile=cover_router.out
-
-  go tool cover -html=cover_model.out -o cover_model.html
-  go tool cover -html=cover_router.out -o cover_router.html
+  go test ./usecase ./handler ./repository -v -covermode=atomic -coverprofile=cover.out
+  go tool cover -html=cover.out -o cover.html
 else
   echo "Running Tests..."
 
-  go test . ./model -v -covermode=atomic
-  go test . ./router -v -covermode=atomic
+  go test ./usecase ./handler ./repository -v
 fi
-
-docker compose -f docker/test/docker-compose.yml down
