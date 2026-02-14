3c62d50abf4da745016e586319a25f07c2f888b1
yas-ako <105139975+yas-ako@users.noreply.github.com>
2026-02-15 00:31:37 +0900

    feat: main関数に、CommentのRepositoryとUsecaseを追加, Commentをデータベースに追加

--- main.go
+++ main.go
@@ -39,6 +39,7 @@ func main() {
 
 	// Repository
 	itemRepo := repository.NewItemRepository(db)
+	commentRepo := repository.NewCommentRepository(db)
 	fileRepo := repository.NewFileRepository(db)
 	ownershipRepo := repository.NewOwnershipRepository(db)
 	transactionRepo := repository.NewTransactionRepository(db)
@@ -50,6 +51,7 @@ func main() {
 
 	// UseCase
 	itemUseCase := usecase.NewItemUseCase(itemRepo)
+	commentUsecase := usecase.NewCommentUsecase(commentRepo, itemRepo)
 	fileUseCase := usecase.NewFileUseCase(fileRepo, fileStorage)
 	ownershipUseCase := usecase.NewOwnershipUseCase(ownershipRepo)
 	borrowingUseCase := usecase.NewBorrowingUseCase(transactionRepo, ownershipRepo)
