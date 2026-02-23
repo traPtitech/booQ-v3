8ed4355c16b3243167469cd2269da5efa85f06b9
kavos113 <kohei.hadano@gmail.com>
2026-02-23 20:54:23 +0900

    feat: item usecase

--- usecase/item.go
+++ usecase/item.go
@@ -1,10 +1,17 @@
 package usecase
 
-import "github.com/traPtitech/booQ-v3/domain"
+import (
+	"fmt"
+
+	"github.com/traPtitech/booQ-v3/domain"
+)
 
 type ItemUseCase interface {
 	GetItemByID(id int) (*domain.Item, error)
-	// TODO: other methods
+	SearchItems(query domain.ItemSearchQuery) ([]*domain.Item, error)
+	CreateItem(item *domain.Item) (*domain.Item, error)
+	UpdateItem(item *domain.Item) (*domain.Item, error)
+	DeleteItem(id int) error
 }
 
 type itemUseCase struct {
@@ -19,4 +26,38 @@ func NewItemUseCase(itemRepo domain.ItemRepository) ItemUseCase {
 
 func (u *itemUseCase) GetItemByID(id int) (*domain.Item, error) {
 	return u.itemRepo.GetByID(id)
+}
+
+func (u *itemUseCase) SearchItems(query domain.ItemSearchQuery) ([]*domain.Item, error) {
+	if query.Offset > 0 && query.Limit <= 0 {
+		return nil, fmt.Errorf("%w: offset is set but limit is not set", ErrInvalidSearchQuery)
+	}
+
+	return u.itemRepo.Search(query)
+}
+
+func (u *itemUseCase) CreateItem(item *domain.Item) (*domain.Item, error) {
+	return u.itemRepo.Create(item)
+}
+
+// TODO: updateの認可
+func (u *itemUseCase) UpdateItem(item *domain.Item) (*domain.Item, error) {
+	itemOld, err := u.itemRepo.GetByID(item.ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get item: %w", err)
+	}
+
+	if itemOld.BookDetail == nil && item.BookDetail != nil || itemOld.BookDetail != nil && item.BookDetail == nil {
+		return nil, fmt.Errorf("%w: cannot change whether item is book or not", ErrUpdateNotAllowed)
+	}
+	if itemOld.EquipmentDetail == nil && item.EquipmentDetail != nil || itemOld.EquipmentDetail != nil && item.EquipmentDetail == nil {
+		return nil, fmt.Errorf("%w: cannot change whether item is equipment or not", ErrUpdateNotAllowed)
+	}
+
+	return u.itemRepo.Update(item)
+}
+
+// TODO: こっちも認可
+func (u *itemUseCase) DeleteItem(id int) error {
+	return u.itemRepo.Delete(id)
 }
